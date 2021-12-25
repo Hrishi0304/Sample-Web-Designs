350edc3e72973f264c6105793f8e3fa7a546703f
Hrishikesh <83577198+Hrishi0304@users.noreply.github.com>
2021-12-25 15:13:52 +0530

    Update resp.js

--- js/resp.js
+++ js/resp.js
@@ -2,8 +2,15 @@ burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 navList = document.querySelector('.nav-list')
 rightNav = document.querySelector('.rightNav')
+//For Toggle-view
 burger.addEventListener('click', ()=>{
     rightNav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp')
 })
+//nav button transition
+const nav = document.getElementById("nav-bar");
+const hamburger = document.getElementById("hamburger-menu");
+hamburger.addEventListener('click',()=>{
+    nav.classList.toggle('change');
+});
